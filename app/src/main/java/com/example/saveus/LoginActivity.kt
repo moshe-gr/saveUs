c561169fb01d28074dd18f4af8443b852a039829
student <stu@ravtech.local>
2021-08-26 17:54:02 +0300

    cancel tool bar

--- app/src/main/java/com/example/saveus/LoginActivity.kt
+++ app/src/main/java/com/example/saveus/LoginActivity.kt
@@ -7,5 +7,7 @@ class LoginActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_login)
+
+        setSupportActionBar(findViewById(R.id.my_toolbar))
     }
 }
