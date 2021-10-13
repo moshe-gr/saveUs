918c394d192e3595fcebfea7d5ee1f9d745ffa33
moshe-gr <mosegreenberg6@gmail.com>
2021-10-13 18:21:08 +0300

    profile

---
+++ app/src/main/java/com/example/saveus/PersonalInfo.kt
@@ -0,0 +1,11 @@
+package com.example.saveus
+
+import java.util.*
+
+class PersonalInfo(
+    var firstName: String,
+    var surName: String,
+    var email: String,
+    var phoneNumber: String,
+    var birthDate: Date,
+    var allowNotifications: Boolean)
